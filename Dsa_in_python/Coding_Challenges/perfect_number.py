4cabdece997895c6a9fbf8ecba62b8d10d084509
Niranjanakamaraj <niranjanakamaraj03@gmail.com>
2025-10-12 00:27:19 +0530

    optimized solution to find a perfect number

--- Dsa_in_python/Coding_Challenges/perfect_number.py
+++ Dsa_in_python/Coding_Challenges/perfect_number.py
@@ -1,11 +1,11 @@
 class Solution:
     def isPerfect(self, n):
         a=0
-        b=n//2+1
-        for i in range(1,b):
+        b=int(n**0.5)
+        for i in range(1,b+1):
             if n%i==0:
-                a+=i
-        if a==n:
+                a+=i+(n//i)
+        if (a-n)==n:
             return True
         else:
             return False
