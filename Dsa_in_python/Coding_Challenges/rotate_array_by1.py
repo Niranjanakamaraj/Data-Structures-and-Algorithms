e25b4131309b1c51154daaf652b287784c2a4540
Niranjanakamaraj <niranjanakamaraj03@gmail.com>
2025-10-18 00:07:48 +0530

    # This program rotates the array by 1 position to the right in O(n) Time Complexity

---
+++ Dsa_in_python/Coding_Challenges/rotate_array_by1.py
@@ -0,0 +1,8 @@
+# This program rotates the array by 1 position to the right
+class Solution:
+    def rotate(self, arr):
+        temp=arr[len(arr)-1]
+        for i in range(len(arr)-1,0,-1):
+            arr[i]=arr[i-1]
+        arr[0]=temp
+        return arr
