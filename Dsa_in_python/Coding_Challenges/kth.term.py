79b0cd16b0c3b49e9c8fa7ea0b51fed27dba32fb
Niranjanakamaraj <niranjanakamaraj03@gmail.com>
2025-10-28 23:24:53 +0530

    Thiis program finds the kth term from the merged sorted array of two sorted arrays in O(n) Time Complexity .

--- Dsa_in_python/Coding_Challenges/kth.term.py
+++ Dsa_in_python/Coding_Challenges/kth.term.py
@@ -1,4 +1,12 @@
 class Solution:
     def kthElement(self, a, b, k):
-        c=sorted(a+b)
-        return c[k-1]
+        i = j = 0
+        while k > 0:
+            if i < len(a) and (j >= len(b) or a[i] <= b[j]):
+                val = a[i]
+                i += 1
+            else:
+                val = b[j]
+                j += 1
+            k -= 1
+        return val
