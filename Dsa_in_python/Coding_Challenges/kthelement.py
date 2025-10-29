2051195898decfa3953b3561cb29f309150c478b
Niranjanakamaraj <niranjanakamaraj03@gmail.com>
2025-10-29 20:45:46 +0530

    This program finds kth element in 2 sorted array in O(n) Time Complexity .

--- Dsa_in_python/Coding_Challenges/kthelement.py
+++ Dsa_in_python/Coding_Challenges/kthelement.py
@@ -1,12 +1,12 @@
 class Solution:
     def kthElement(self, a, b, k):
-        i = j = 0
-        while k > 0:
-            if i < len(a) and (j >= len(b) or a[i] <= b[j]):
-                val = a[i]
-                i += 1
+        i,j=0,0
+        while k>0:
+            if i<len(a) and (j>=len(b) or a[i]<=b[j]) :
+                res=a[i]
+                i+=1
             else:
-                val = b[j]
-                j += 1
-            k -= 1
-        return val
+                res=b[j]
+                j+=1
+            k-=1
+        return res
