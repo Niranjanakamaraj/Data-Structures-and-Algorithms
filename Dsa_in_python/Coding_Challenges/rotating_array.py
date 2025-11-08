1e0748af26bf62fc3542a52f19f50fa35a651c97
Niranjanakamaraj <niranjanakamaraj03@gmail.com>
2025-11-08 23:37:25 +0530

    This program rotates the array inplace in O(n) Tc O(1) Sc.

---
+++ Dsa_in_python/Coding_Challenges/rotating_array.py
@@ -0,0 +1,11 @@
+def rotate(self,nums,k):
+    k%=len(nums)
+    def rev(i,j);
+        while i<j:
+            nums[i],nums[j]=nums[j],nums[i]
+            i+=1
+            j-=1
+    rev(0,len(nums)-1
+    rev(0,k-1)
+    rev(k,len(nums)-1))
+    return nums
