1d9249f086518fe7ab7177faaa19f5643fdfc68f
Niranjanakamaraj <niranjanakamaraj03@gmail.com>
2026-01-06 22:52:41 +0530

    This program finds the number of subarray with sum k in O(n)Tc and O(n)Sc

--- Dsa_in_python/Coding_Challenges/count_longest_subarray_k.py
+++ Dsa_in_python/Coding_Challenges/count_longest_subarray_k.py
@@ -1,11 +1,11 @@
-c_s,count=0,0
-a={0:1}
+p_s,count=0,0
+d={0:1}
 for i in range(len(nums)):
-    c_s+=nums[i]
-    if c_s-k in a:
-        count+=a[c_s-k]
-    if c_s in a:
-        a[c_s]+=1
-    else:
-        a[c_s]=1                
+    p_s+=nums[i]
+    if p_s-k in d:
+        count+=d[p_s-k]
+        if p_s in d:
+            d[p_s]+=1
+        else:
+            d[p_s]=1
 return count
