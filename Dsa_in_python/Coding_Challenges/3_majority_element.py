06a3bbc92d89a1592dbc9c7f4860d937c8e8c46d
Niranjanakamaraj <niranjanakamaraj03@gmail.com>
2026-01-09 23:57:48 +0530

    Optimized code with O(n)Tc and O(1) Sc

--- Dsa_in_python/Coding_Challenges/3_majority_element.py
+++ Dsa_in_python/Coding_Challenges/3_majority_element.py
@@ -1,10 +1,22 @@
-d={}
-a=[]
+count1,count2=0,0
+a,b=float('-inf'),float('-inf')
 for i in range(len(nums)):
-    if nums[i] in d:
-        d[nums[i]]+=1
+    if count1==0 and nums[i]!=a:
+        a=nums[i]
+        count1=1
+    elif count2==0 and nums[i]!=b:
+        b=nums[i]
+        count2=1
+    elif nums[i]==a:
+        count1+=1
+    elif nums[i]==b:
+        count2+=1
     else:
-        d[nums[i]]=1
-    if d[nums[i]] >len(nums)/3 and nums[i] not in a:
-        a.append(nums[i])
-return a
+        count1-=1
+        count2-=1
+r=[]
+if nums.count(a)>len(nums)//3:
+    r.append(a)
+elif b!=a and count1>len(nums):
+    r.append(b)
+return r
