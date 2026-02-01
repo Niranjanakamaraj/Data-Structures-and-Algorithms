967129c2a95658770037e248a45c617897cd84e4
Niranjanakamaraj <niranjanakamaraj03@gmail.com>
2026-02-01 08:58:00 +0530

    THis program finds the index of search element if present in the array in O(log n)Tc and O(1)Sc which is b_search technique the condition is the array has to be sorted

---
+++ Dsa_in_python/Coding_Challenges/b_search.py
@@ -0,0 +1,10 @@
+l,r=0,len(nums)-1
+        while l<=r:
+            m=(l+r)//2
+            if target==nums[m]:
+                return m
+            elif target>nums[m]:
+                l=m+1
+            else:
+                r=m-1
+        return -1
