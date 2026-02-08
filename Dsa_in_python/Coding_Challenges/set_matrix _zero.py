45c3b8792b3b7d8f38f82de6d6104af4ecb1d70d
Niranjanakamaraj <niranjanakamaraj03@gmail.com>
2026-02-08 23:44:14 +0530

    This program set the matrix to zeroa in O(mn)Tc and O(1) Sc

--- Dsa_in_python/Coding_Challenges/set_matrix _zero.py
+++ Dsa_in_python/Coding_Challenges/set_matrix _zero.py
@@ -1,8 +1,6 @@
 class Solution:
     def setZeroes(self, matrix):
-        m = len(matrix)
-        n = len(matrix[0])
-
+        m, n = len(matrix), len(matrix[0])
         firstRowZero = False
         firstColZero = False
 
@@ -23,13 +21,8 @@ class Solution:
                     matrix[0][j] = 0
 
         for i in range(1, m):
-            if matrix[i][0] == 0:
-                for j in range(1, n):
-                    matrix[i][j] = 0
-
-        for j in range(1, n):
-            if matrix[0][j] == 0:
-                for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
                     matrix[i][j] = 0
 
         if firstRowZero:
