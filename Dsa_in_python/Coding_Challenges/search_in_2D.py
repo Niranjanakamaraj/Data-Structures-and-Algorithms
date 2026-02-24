b1039c1a74edc750e064d3233e77ea1f60a0c2bf
Niranjanakamaraj <niranjanakamaraj03@gmail.com>
2026-02-24 22:46:25 +0530

    This program finds element is 2D array in O(lof(m*n)TC And O(1)Sc

--- Dsa_in_python/Coding_Challenges/search_in_2D.py
+++ Dsa_in_python/Coding_Challenges/search_in_2D.py
@@ -1,5 +1,17 @@
-for i in range(len(matrix)):
-    for j in range(len(matrix[0])):
-        if matrix[i][j]==target:
-            return True
+if not matrix or not matrix[0]:
+    return False
+m=len(matrix)
+n=len(matrix[0])
+l,r=0,m*n-1
+while l<=r:
+    mid=(l+r)//2
+    row=mid//n
+    col=mid%n
+    element=matrix[row][col]
+    if element==target:
+        return True
+    elif element<target:
+        l=mid+1
+    else:
+        r=mid-1
 return False
