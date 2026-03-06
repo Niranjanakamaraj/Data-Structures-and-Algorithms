daa671b6287b5d3bc8c479fe21aebdcaca5f1f98
Niranjanakamaraj <niranjanakamaraj03@gmail.com>
2026-03-06 23:52:42 +0530

    This program finds pow in O(log n)Tc and O(1) Sc

--- Dsa_in_python/Coding_Challenges/pow(x.,n).py
+++ Dsa_in_python/Coding_Challenges/pow(x.,n).py
@@ -1,6 +1,13 @@
-def pw(x,n):
-    while n > 0:
-    if n % 2 == 1:
-        result *= x
-    x *= x
-    n //= 2
+def pow(x,n)
+if n==0:
+     return 1
+    elif n<0:
+        x=1/x
+    n=-n
+    r=1
+    while n>0:
+        if n%2==1:
+            r*=x
+        x*=x
+        n//=2
+return r
