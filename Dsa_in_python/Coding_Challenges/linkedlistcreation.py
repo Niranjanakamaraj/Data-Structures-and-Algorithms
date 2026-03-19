11dd2fe6d43ac2ca06c139196b45a7e541f3226b
Niranjanakamaraj <niranjanakamaraj03@gmail.com>
2026-03-19 23:54:20 +0530

    linked list creation

---
+++ Dsa_in_python/Coding_Challenges/linkedlistcreation.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+n1 = Node(1)
+n2 = Node(2)
+n3 = Node(3)
+
+n1.next = n2
+n2.next = n3
+
+curr = n1
+while curr:
+    print(curr.data, end=" ")
+    curr = curr.next
