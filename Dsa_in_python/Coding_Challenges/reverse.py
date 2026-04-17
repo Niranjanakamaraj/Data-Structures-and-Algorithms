e4d97cd1dea836365e926194a6b99bdfaef8b5ba
Niranjanakamaraj <niranjanakamaraj03@gmail.com>
2026-04-17 23:49:05 +0530

    Linked list reversal

--- Dsa_in_python/Coding_Challenges/reverse.py
+++ Dsa_in_python/Coding_Challenges/reverse.py
@@ -3,12 +3,12 @@ class ListNode:
         self.val = val
         self.next = next
 
-def reverseList(head):
-    prev = None
-    curr = head
-    while curr:
-        nxt = curr.next
-        curr.next = prev
-        prev = curr
-        curr = nxt
-    return prev
+    def reverseList(head):
+        prev = None
+        curr = head
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        return prev
