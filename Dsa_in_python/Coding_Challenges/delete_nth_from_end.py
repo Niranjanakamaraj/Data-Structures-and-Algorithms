092f9141c8b73a7184de13ed5633d790c1578277
Niranjanakamaraj <niranjanakamaraj03@gmail.com>
2026-05-01 20:43:19 +0530

    This program removes the nth node from the end of the linked list and returns the head of the modified linked list which uses Hare and Tortoise Algorithm in O(N) Tc and O(1) Sc

--- Dsa_in_python/Coding_Challenges/delete_nth_from_end.py
+++ Dsa_in_python/Coding_Challenges/delete_nth_from_end.py
@@ -1,20 +1,15 @@
 class Solution(object):
     def removeNthFromEnd(self, head, n):
-        res=ListNode(0)
+        fast=head
         count=0
-        temp=head
-        while temp:
-            temp=temp.next
+        while count<n:
+            fast=fast.next
             count+=1
-        pos=(count-n)
-        if pos==0:
+        if not fast:
             return head.next
-        temp=head
-        count=0
-        while temp:
-            count+=1
-            if count==pos:
-                temp.next=temp.next.next
-            else:
-                temp=temp.next
-        return head
+        slow=head
+        while fast.next:
+            fast=fast.next
+            slow=slow.next
+        slow.next=slow.next.next
+    return head
