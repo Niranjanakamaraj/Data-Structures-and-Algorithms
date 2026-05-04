b5406a6f1a2f440b66d062e725db873e92c78460
Niranjanakamaraj <niranjanakamaraj03@gmail.com>
2026-05-04 21:11:50 +0530

    count

--- Dsa_in_python/Coding_Challenges/count_indices_value_with_opposite.py
+++ Dsa_in_python/Coding_Challenges/count_indices_value_with_opposite.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def countOppositeParity(self, nums):
         odd,even=0,0
-        res=[]
+        result=[]
         for i in range(len(nums)):
             if nums[i]%2==0:
                 even+=1
@@ -10,8 +10,8 @@ class Solution(object):
         for i in range(len(nums)):
             if nums[i]%2==0:
                 even-=1
-                res.append(odd)
+                result.append(odd)
             else:
                 odd-=1
-                res.append(even)
-        return res
+                result.append(even)
+        return result
