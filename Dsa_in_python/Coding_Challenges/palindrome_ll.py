3599cebeec6eba2357c828cda708fcbb4befe229
Niranjanakamaraj <niranjanakamaraj03@gmail.com>
2026-05-25 23:50:30 +0530

    better approach

--- Dsa_in_python/Coding_Challenges/palindrome_ll.py
+++ Dsa_in_python/Coding_Challenges/palindrome_ll.py
@@ -1,16 +1,25 @@
-    def isPalindrome(self, head):   
-        arr = []
-        while head:
-            arr.append(head.val)
-            head = head.next
-        l = 0
-        r = len(arr) - 1
+class Solution(object):
+    def isPalindrome(self, head):
 
-        while l < r:
-            if arr[l] != arr[r]:
+        arr = []
+
+        curr = head
+
+        # store linked list values
+        while curr:
+            arr.append(curr.val)
+            curr = curr.next
+
+        left = 0
+        right = len(arr) - 1
+
+        # compare from both sides
+        while left < right:
+
+            if arr[left] != arr[right]:
                 return False
 
-            l += 1
-            r -= 1
+            left += 1
+            right -= 1
 
         return True
