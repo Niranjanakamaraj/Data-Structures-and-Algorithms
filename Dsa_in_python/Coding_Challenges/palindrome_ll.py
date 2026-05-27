2c376d8e28312e98075a4ca7e34b6ca7778e5a6a
Niranjanakamaraj <niranjanakamaraj03@gmail.com>
2026-05-27 23:54:55 +0530

    Alignment

--- Dsa_in_python/Coding_Challenges/palindrome_ll.py
+++ Dsa_in_python/Coding_Challenges/palindrome_ll.py
@@ -1,25 +1,15 @@
 class Solution(object):
     def isPalindrome(self, head):
-
         arr = []
-
         curr = head
-
-        # store linked list values
         while curr:
             arr.append(curr.val)
             curr = curr.next
-
         l = 0
         r = len(arr) - 1
-
-        # compare from both sides
         while l < r:
-
             if arr[l] != arr[r]:
                 return False
-
             l += 1
             r -= 1
-
         return True
